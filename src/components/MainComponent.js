9660b9a09a90b2175918ff37726e2e7d205f399f
CuongNHT <nhtcit99@gmail.com>
2022-05-18 12:51:14 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -45,6 +45,24 @@ class Main extends Component {
 				/>
 			);
 		};
+
+		const DishWithId = ({ match }) => {
+			return (
+				<DishDetail
+					dish={
+						this.state.dishes.filter(
+							(dish) =>
+								dish.id === parseInt(match.params.dishId, 10)
+						)[0]
+					}
+					comments={this.state.comments.filter(
+						(comment) =>
+							comment.dishId === parseInt(match.params.dishId, 10)
+					)}
+				/>
+			);
+		};
+
 		return (
 			<div>
 				<Header />
@@ -55,6 +73,7 @@ class Main extends Component {
 						path="/menu"
 						component={() => <Menu dishes={this.state.dishes} />}
 					/>
+					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
