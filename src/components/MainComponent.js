0fcac416ffeebae3a7aa160130def74c494ff3c5
CuongNHT <nhtcit99@gmail.com>
2022-05-18 10:05:50 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,12 @@ import React, { Component } from "react";
 import Home from "./HomeComponent";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
@@ -11,7 +15,12 @@ import { Switch, Route, Redirect } from "react-router-dom";
 class Main extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { dishes: DISHES, selectedDish: null };
+		this.state = {
+			dishes: DISHES,
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS,
+		};
 	}
 
 	onDishSelect(dishId) {
@@ -20,7 +29,21 @@ class Main extends Component {
 
 	render() {
 		const HomePage = () => {
-			return <Home />;
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={
+						this.state.promotions.filter(
+							(promo) => promo.featured
+						)[0]
+					}
+					leader={
+						this.state.leaders.filter(
+							(leader) => leader.featured
+						)[0]
+					}
+				/>
+			);
 		};
 		return (
 			<div>
@@ -32,6 +55,7 @@ class Main extends Component {
 						path="/menu"
 						component={() => <Menu dishes={this.state.dishes} />}
 					/>
+					<Route exact path="/contactus" component={Contact} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
