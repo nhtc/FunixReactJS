d6d792d18a1c21aeeb8157e68e624bb33596ca80
CuongNHT <nhtcit99@gmail.com>
2022-06-05 01:21:48 +0700

    Controlled Forms

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.css";
-import "bootstrap-social/bootstrap-social.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "font-awesome/css/font-awesome.css"
+import "bootstrap-social/bootstrap-social.css"
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
@@ -13,8 +12,7 @@ import "bootstrap-social/bootstrap-social.css";
 // 		<App />
 // 	</React.StrictMode>
 // );
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
