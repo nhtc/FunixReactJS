1ac3bb7ce18c84ad218a8764082ce245ada7475c
CuongNHT <nhtcit99@gmail.com>
2022-06-05 18:22:19 +0700

    update validate onChange instead of onBlur

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Main from "./components/MainComponent";
-import { STAFFS } from "./shared/staffs";
-import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import Main from './components/MainComponent';
+import { STAFFS } from './shared/staffs';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			staffs: STAFFS,
-			columns: 4,
-		};
-	}
-	onSetColumn = (column) => {
-		console.log(column);
-		this.setState({ columns: column });
-	};
-	render() {
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<Main />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+      columns: 4,
+    };
+  }
+  onSetColumn = (column) => {
+    console.log(column);
+    this.setState({ columns: column });
+  };
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
