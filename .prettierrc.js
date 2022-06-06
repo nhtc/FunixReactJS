f1bb76feaf6b1449beddcaaaeb6ba005b771960f
CuongNHT <nhtcit99@gmail.com>
2022-06-06 15:10:21 +0700

    update prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    printWidth: 80,
-    tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  singleAttributePerLine: false,
 };
