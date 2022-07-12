70f09d8a5efbf423b1bab6f9df5288eba3d32676
CuongNHT <nhtcit99@gmail.com>
2022-07-12 08:13:56 +0700

    search with uncontrolled  form

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -2,45 +2,46 @@ import React, { useEffect, useState } from "react"
 import useDebounce from "./Debounce"
 
 const Search = ({ staff, staffList }) => {
-	const [searchTerm, setSearchTerm] = useState("")
-
-	const debounceSearch = useDebounce(searchTerm, 300)
-
-	useEffect(() => {
-		if (debounceSearch) {
-			staffList(
-				staff.filter(s =>
-					s.name.toLowerCase().includes(debounceSearch.toLowerCase())
-				)
-			)
-		} else {
-			staffList(null)
-		}
-	}, [debounceSearch])
-
+	const handleSubmit = e => {
+		e.preventDefault()
+		const searchTerm = e.target.search.value
+		console.log("value of form", searchTerm)
+		searchTerm
+			? staffList(
+					staff.filter(s =>
+						s.name.toLowerCase().includes(searchTerm.toLowerCase())
+					)
+			  )
+			: staffList(null)
+	}
 	return (
-		<div>
-			<div className='input-group'>
-				<input
-					type='text'
-					className='form-control rounded'
-					placeholder='Search name of staff ...'
-					aria-label='Search'
-					aria-describedby='search-addon'
-					value={searchTerm}
-					onChange={e => setSearchTerm(e.target.value)}
-					style={{
-						maxWidth: "500px",
-						margin: "5px auto",
-						border: "5px solid red",
-						borderRadius: "15px",
-					}}
-				/>
-				{/* <button type="button" className="btn btn-outline-primary">
-          Search
-        </button> */}
-			</div>
-		</div>
+		<form
+			onSubmit={handleSubmit}
+			className='input-group'
+			style={{
+				maxWidth: "500px",
+				margin: "5px auto",
+				borderRadius: "15px",
+			}}
+		>
+			<input
+				type='text'
+				className='form-control rounded'
+				placeholder='Search name of staff ...'
+				aria-label='Search'
+				name='search'
+				aria-describedby='search-addon'
+			/>
+			<span>
+				<button
+					type='submit'
+					value='submit'
+					className='btn btn-outline-primary'
+				>
+					Search
+				</button>
+			</span>
+		</form>
 	)
 }
 
